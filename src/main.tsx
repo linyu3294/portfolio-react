8e4be637ebdc8c1baf95ded3a25519c59a76998b
Yu Lin <yu@mavrck.co>
2024-03-04 00:04:43 -0500

    Think it worked this time?

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// Since I had to downgrade to React 16.8.0 I can no longer use the createRoot method
+// Use the following if upgrading to React 18 or higher
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
+
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root')
 );
