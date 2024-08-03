d9abf9bf7c1d2949cd762f1ad1adb4b133b3fecb
linyu3294 <yu.lin@later.com>
2024-08-03 13:48:20 -0400

    Added songs

--- src/components/pages/music.page.tsx
+++ src/components/pages/music.page.tsx
@@ -6,11 +6,77 @@ const Music: React.FC = () => {
 
     return (
         <div className="page-container">
-        <h3> Remake of Luma Theme - Super Mario Galaxy </h3>
-
+            <h3> Remake of Luma Theme - Super Mario Galaxy </h3>
             <AudioPlayer
                 autoPlay={false}
                 src="https://kxf-s3-music.s3.amazonaws.com/Luma's+Lullaby+-+Mario+Galaxy.mp3"
+                onPlay={ e => console.log(`There is an error loading this song, ${e}`)}
+                autoPlayAfterSrcChange={false}
+            />
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <h3> Another Core - Undertale (MIT Video Game Orchestra)</h3>
+            <AudioPlayer
+                autoPlay={false}
+                src="https://kxf-s3-music.s3.amazonaws.com/vgo-another-core-undertale.wav"
+                onPlay={ e => console.log(`There is an error loading this song, ${e}`)}
+                autoPlayAfterSrcChange={false}
+            />
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <h3> National Park - Pokemon HGSS (MIT Video Game Orchestra)</h3>
+            <AudioPlayer
+                autoPlay={false}
+                src="https://kxf-s3-music.s3.amazonaws.com/vgo-national-park-pokemon-hgss.mp3"
+                onPlay={ e => console.log(`There is an error loading this song, ${e}`)}
+                autoPlayAfterSrcChange={false}
+            />
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <h3> Homage to Bach - Two Fugues and Preludes</h3>
+            <AudioPlayer
+                autoPlay={false}
+                src="https://kxf-s3-music.s3.amazonaws.com/homage-to-bach-two-fugues-and-preludes.mp3"
+                onPlay={ e => console.log(`There is an error loading this song, ${e}`)}
+                autoPlayAfterSrcChange={false}
+            />
+
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <h3> Faded </h3>
+            <AudioPlayer
+                autoPlay={false}
+                src="https://kxf-s3-music.s3.amazonaws.com/faded.wav"
+                onPlay={ e => console.log(`There is an error loading this song, ${e}`)}
+                autoPlayAfterSrcChange={false}
+            /> 
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <h3> Orchestral Piece </h3>
+            <AudioPlayer
+                autoPlay={false}
+                src="https://kxf-s3-music.s3.amazonaws.com/orchestral-piece.wav"
+                onPlay={ e => console.log(`There is an error loading this song, ${e}`)}
+                autoPlayAfterSrcChange={false}
+            />
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <h3> Credits Theme </h3>
+            <AudioPlayer
+                autoPlay={false}
+                src="https://kxf-s3-music.s3.amazonaws.com/credits-theme.wav"
                 onPlay={ e => console.log(`There is an error loading this song, ${e}`)}
                 autoPlayAfterSrcChange={false}
             />
